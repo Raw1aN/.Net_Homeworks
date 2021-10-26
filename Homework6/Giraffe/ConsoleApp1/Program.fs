d9109932c8b98a0c74508e130391b8d7afbda83c
Alexey <shchukin2002@yandex.ru>
2021-10-26 15:11:25 +0300

    add

---
+++ Homework6/Giraffe/ConsoleApp1/Program.fs
@@ -0,0 +1,25 @@
+
+
+open System
+open FSharp.Data
+
+let doRequest=
+    async {
+        let! r1 = Http.AsyncRequest "http://localhost:5000/add?v1=1&v2=2"
+        let str =
+            match r1.Body with
+            |HttpResponseBody.Text t -> t
+            |HttpResponseBody.Binary _ -> "Binary"
+        return str 
+    }
+
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let r = doRequest |> Async.RunSynchronously
+    
+    printfn $"{r}"
+    
+    0 
---
+++ Homework6/Giraffe/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+        static async Task DoRequest()
+        {
+            var httpClient = new HttpClient();
+
+            var response = await httpClient
+                .GetAsync("http://localhost:5000/add?v1=1&v2=2");
+            var str = await response.Content
+                .ReadAsStringAsync();
+
+            Console.WriteLine(str);
+        }
+        
+        await DoRequest();
