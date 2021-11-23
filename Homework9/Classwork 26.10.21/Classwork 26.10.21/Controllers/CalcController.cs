661054f806bee49fe1824456fb74d8ffd366ece9
Alexey <shchukin2002@yandex.ru>
2021-11-23 13:09:08 +0300

    Hw9

---
+++ Homework9/Classwork 26.10.21/Classwork 26.10.21/Controllers/CalcController.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using Classwork_26._10._21.Models.Calculator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Classwork_26._10._21.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Calculate([FromServices] ICalculator calc, string s)
+        {
+            return Content("0");
+            //https://localhost:5001/calc/calculate?s= (2+3) / 12 * 7 + 8 * 9
+        }
+    }
+}
---
+++ Homework9/Classwork 26.10.21/Classwork 26.10.21/Models/Calculator/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork_26._10._21.Models.Calculator
+{
+    public enum Token
+    {
+        EOF,
+        Add,
+        Subtract,
+        Number,
+    }
+}
