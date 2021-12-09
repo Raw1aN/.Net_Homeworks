f87a56eb6618d5854c39c9240f5bbe09707115c3
Alexey <shchukin2002@yandex.ru>
2021-12-09 14:15:35 +0300

    Hw11

---
+++ Homework11/Classwork 26.10.21/Classwork 26.10.21/Models/Calculator/ICalculator.cs
@@ -0,0 +1,9 @@
+﻿using Classwork_26._10._21.Models.Calculator;
+namespace Classwork_26._10._21.Models.Calculator
+
+{
+    public interface ICalculator
+    {
+        string Calculate(string val1, string operation, string val2);
+    }
+}
