eca7e8df3468131088029e11dd76e0e19780fda4
Alexey <shchukin2002@yandex.ru>
2021-12-30 04:39:11 +0300

    add Homework12

---
+++ Homework12/HW12/HW12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+
+namespace HW12
+{
+    class Program
+    {
+        static ManualConfig config = new ManualConfig()
+            .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+            .AddValidator(JitOptimizationsValidator.DontFailOnError)
+            .AddLogger(ConsoleLogger.Default)
+            .AddColumnProvider(DefaultColumnProviders.Instance);
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>(config);
+        }
+    }
+}
